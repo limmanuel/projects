9a03c1ecbc9c5b8eab41eeccf9803de18affe314
Limmanuel <limmanuellopez@gmail.com>
2017-04-25 09:58:00 +0800

    portfolio

---
+++ portfolio/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+var contact = require('./html/contact')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/contact', contact);
+app.get('/', function(req,res){
+	res.redirect('/index.html')
+});
+
+app.use(express.static(path.join(__dirname,'html')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/index.html'));
+});
+app.use('/download', function(req, res) {
+	res.download(path.join(__dirname + '/download/Limmanuel-B.-Lopez-Resume.docx'))
+});
+
+app.listen(3000,function(){
+	console.log("starting");
+});
+
+module.exports = app;
---
+++ portfolio/html/contact.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var router = express.Router();
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+router.get('/', function(req, res, next) {
+  res.render('contact', { 
+  	title: 'Contact' 
+  });
+});
+router.post('/send', function(req,ress){
+	console.log("sending");
+	var transporter = nodemailer.createTransport({
+		service:'Gmail',
+		auth: {
+			user: 'limmanuellopez@gmail.com',
+			pass: 'cj3u7hx3t'
+		}
+	});
+
+	var mailOptions = {
+		from: '"Limmanuel Lopez ?" <limmanuellopez@gmail.com>',
+		to: 'lopezlimmanuel@gmail.com',
+		subject: 'testing my website',
+		text: 'Message from your HTML Name: '+ req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message ,
+		html: '<p>Message from your HTML </p><ul><li>Name: '+ req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+	}
+	transporter.sendMail(mailOptions,function(error,info){
+		if(error){return console.log(error);}
+		console.log('message Sent', info.response)
+		res.redirect('/');
+	});
+});
+
+ 
+module.exports = router;
